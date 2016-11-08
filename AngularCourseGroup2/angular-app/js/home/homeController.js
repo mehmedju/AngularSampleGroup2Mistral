73c6e5e9b63c0e3bee1803aff24a629c257db881
mehmedju <mehmedju@maestralsolutions.com>
2016-11-08 11:14:38 +0100

    Home Screen

--- AngularCourseGroup2/angular-app/js/home/homeController.js
+++ AngularCourseGroup2/angular-app/js/home/homeController.js
@@ -1,5 +1,45 @@
 ﻿angular.module('app')
     .controller('homeController', ['$scope', function ($scope) {
         'use strict';
+
+        $scope.selectedParticipant = {};
+        $scope.allParticipants = [
+            {
+                id: 1,
+                name: "Zinajda",
+                surname: "Sarac",
+                position: "QA",
+                team: "ESecuritel",
+                hireDate: new Date(2010, 9, 11)
+            },
+            {
+                id: 2,
+                name: "Enes",
+                surname: "Kuhn",
+                position: "QA",
+                team: "ESecuritel",
+                hireDate: new Date(2010, 6, 11)
+            },
+            {
+                id: 3,
+                name: "Ammar",
+                surname: "Kevilj",
+                position: "QA",
+                team: "ESecuritel",
+                hireDate: new Date(2013, 9, 18)
+            },
+            {
+                id: 4,
+                name: "Dzenan",
+                surname: "Dizdarevic",
+                position: "QA",
+                team: "AT&T",
+                hireDate: new Date(2011, 7, 21)
+            }
+        ];
+
+        $scope.selectParticipant = function (participant) {
+            $scope.selectedParticipant = participant;
+        }
     }
     ]);
---
+++ AngularCourseGroup2/angular-app/js/home/partials/rightPanelController.js
@@ -0,0 +1,24 @@
+﻿angular.module('app')
+    .controller('rightPanelController', ['$scope', function ($scope) {
+        'use strict';
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            maxDate: new Date(2080, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+
+        $scope.openDatepicker = function () {
+            $scope.datepicker.opened = true;
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        $scope.datepicker = {
+            opened: false
+        };
+
+    }
+    ]);
